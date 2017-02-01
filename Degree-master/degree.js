1723364131934edd0af6f1d42f5da3c2a7ffda2a
EGAguirre <9eaguirre@stpatrick.org>
2017-02-01 11:26:31 -0600

    Add files via upload

---
+++ Degree-master/degree.js
@@ -0,0 +1,26 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [1,1,0,1,0],
+    [1,0,1,0,0],
+    [0,1,0,0,0],
+    [1,0,0,0,1],
+    [0,0,0,1,0]
+]);
+//Code here
+function degree(mtrx,v){
+  let mtrx = potential;
+  let v = vertex;
+  for(sum = 0; v-1; sum++);
+  return sum;
+}
+
+function degreecentrality(){
+
+}
+
+//Challenge
+
+//Run test code with ctrl+shift+p  then type in "script"
+console.log(degree(A,3));
+console.log(degreecentrality(A,2));
